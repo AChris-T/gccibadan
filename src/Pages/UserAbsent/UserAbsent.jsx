9139b473e143c581fe9d55448491cef979d3d0ab
Abiodun-Samuel <abiodunsamyemi@gmail.com>
2024-03-10 04:56:36 +0100

    code update

--- src/Pages/UserAbsent/UserAbsent.jsx
+++ src/Pages/UserAbsent/UserAbsent.jsx
@@ -94,7 +94,7 @@ const UserAbsent = () => {
           className="border-2 my-2 hover:bg-blue-600 border-[#6565ef] px-5 py-2 rounded-lg bg-[blue] md:text-[16px] font-bold text-[white]"
         >
           {loader && <span>Loading...</span>}
-          {!loader && <span> Download Abscent Members List</span>}
+          {!loader && <span> Download Absent Members List</span>}
         </button>
       ) : null}
     </div>
