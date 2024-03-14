5a8eee119b97cf416bd0809417af046c81e12cb8
Abiodun-Samuel <abiodunsamyemi@gmail.com>
2024-03-14 21:34:25 +0100

    updated

--- src/Pages/Attendance/Attendance.jsx
+++ src/Pages/Attendance/Attendance.jsx
@@ -155,14 +155,6 @@ const Attendance = () => {
 
   return (
     <div className="w-full px-2 mt-[50px] h-[100vh] ">
-      <button
-        onClick={handleButtonClick}
-        className="border-2 hover:bg-blue-600 border-[blue] px-5 py-2 rounded-lg bg-[blue] md:text-[16px] font-bold text-[white]"
-      >
-        {loading && <span>Loading...</span>}
-        {!loading && <span> Mark Attendance</span>}
-      </button>
-
       <div className="mb-3">
         <h1 className="mb-2 text-blue-700 text-2xl font-semibold">Beloved, </h1>
         <p className="text-lg">
