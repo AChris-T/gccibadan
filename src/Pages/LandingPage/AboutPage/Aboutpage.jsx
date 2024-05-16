fe6b1bb5d498aef4bb1c2f282cb704912e0b402c
chris <taiwoakinfenwa2019@gmail.com>
2024-05-16 13:04:49 +0100

    about Page updated

--- src/Pages/LandingPage/AboutPage/Aboutpage.jsx
+++ src/Pages/LandingPage/AboutPage/Aboutpage.jsx
@@ -31,7 +31,7 @@ const Aboutpage = () => {
                 <img src={vision} alt="" className="rounded md:w-[500px] md:h-[300px]"/>
               </div>
               <div className="md:w-[458px]">
-                <p className="text-[#ff6347] mb-2">Vision</p>
+                <p className="text-[#ff6347] mb-2 text-center md:text-start">Vision</p>
               <h5 className=""> Vivamus vulputate elit consequat tincidunt maximus. 
                   Vivamus urna erat, egestas nec interdum ut, maximus sed nisi.
                   Donec ac vehicula purus. Fusce sit amet volutpat dolor,
@@ -46,7 +46,7 @@ const Aboutpage = () => {
                 <img src={mission} alt="" className="rounded w-[500px] md:w-[700px] lg:w-[500px] md:h-[300px]"/>
               </div>
               <div className="md:w-[500px]">
-                <p className="text-[#ff6347] mb-2 lg:px-5">Mission</p>
+                <p className="text-[#ff6347] mb-2 lg:px-5  text-center md:text-start">Mission</p>
               <h5 className="lg:w-[500px] md:w-[357px] lg:px-5"> Vivamus vulputate elit consequat tincidunt maximus. 
                   Vivamus urna erat, egestas nec interdum ut, maximus sed nisi.
                   Donec ac vehicula purus. Fusce sit amet volutpat dolor,
